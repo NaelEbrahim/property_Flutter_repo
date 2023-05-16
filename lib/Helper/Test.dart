2a570d66d26b5770ba67a7af9056086a78c47de2
NAEL PC <@naelebrahim42>
2023-05-16 18:04:18 +0200

    test update commit

--- lib/Helper/Test.dart
+++ lib/Helper/Test.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:university_project_property_app/Shared/Components.dart';
 
 class Test extends StatelessWidget {
   const Test({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: reusableText(text: "hello", fontsize: 20),
+    );
   }
 }
+
+
+  //1. checkout master
+  //2. update master
+  //3. create branch from master
+  //4. start coding ( your job )
+  //5. commit
+  //6. re checkout master
+  //7. update master
+  //8. checkout your local branch
+  //9. merge master with my current branch
+  //10. push
+  //11. create pull request
+
+
