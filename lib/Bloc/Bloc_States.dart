88678ea55c75686ac6cddb122c8227ea71d53866
NAEL PC <@naelebrahim42>
2023-05-26 20:04:56 +0200

    finish Chatting by Nael

--- lib/Bloc/Bloc_States.dart
+++ lib/Bloc/Bloc_States.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: camel_case_types, file_names
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 abstract class Bloc_States {}
 
 class Initial_State extends Bloc_States {}
@@ -20,6 +22,16 @@ class LoadingSignupState extends Bloc_States {}
 class SuccessSignupState extends Bloc_States {}
 
 class ErrorSignupState extends Bloc_States {}
+
+//******************* CHATTING STATES *****************//
+class SuccessSendMessageState extends Bloc_States {}
+
+class ErrorSendMessageState extends Bloc_States {}
+
+class SuccessGetMessagesState extends Bloc_States {}
+
+class SuccessReceiveMessagesState extends Bloc_States {}
+
 
 //******************* OTHER STATES *****************//
 class ChangeEyeIcon extends Bloc_States {}
