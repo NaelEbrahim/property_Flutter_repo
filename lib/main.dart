87a450feb44d1b987b29eb73a87e7e089c15213e
NAEL PC <@naelebrahim42>
2023-07-20 13:01:06 +0300

    Search Screen And Some Update By Nael

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.indigo
             ),
             debugShowCheckedModeBanner: false,
-            home: Message_Screen()
+            home: Base_Screen()
           );
         },
       ),
